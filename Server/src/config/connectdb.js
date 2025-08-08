5813c9f56107f7adfa606cc2b540432fbe946766
lytruongsinh123 <diepduong607@gmail.com>
2025-08-08 22:36:48 +0700

    change

--- Server/src/config/connectdb.js
+++ Server/src/config/connectdb.js
@@ -1,6 +1,6 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
-
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+// Option 2: Passing parameters separately (other dialects)
 const sequelize = new Sequelize(
     process.env.DB_DATABASE_NAME,
     process.env.DB_USERNAME,
@@ -11,38 +11,26 @@ const sequelize = new Sequelize(
         dialect: process.env.DB_DIALECT,
         logging: false,
         dialectOptions:
-            process.env.DB_SSL === "true"
-                ? {
-                      ssl: {
-                          require: true,
-                          rejectUnauthorized: false,
-                      },
-                      keepAlive: true,
-                      connectionTimeoutMillis: 30000,
-                  }
-                : {
-                      keepAlive: true,
-                      connectionTimeoutMillis: 30000,
-                  },
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
+            process.env.DB_SSL === 'true' ?
+                {
+                    ssl: {
+                        require: true,
+                        rejectUnauthorized: false
+                    }
+                } : {}
+        ,
         query: {
-            raw: true,
+            "raw": true
         },
-        timezone: "+07:00",
-    }
-);
+        timezone: "+07:00"
+    });
 
 let connectDB = async () => {
     try {
         await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
+        console.log('Connection has been established successfully.');
     } catch (error) {
-        console.error("Unable to connect to the database:", error);
+        console.error('Unable to connect to the database:', error);
     }
-};
+}
 module.exports = connectDB;
